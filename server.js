3714cade20921e08a7aa2667c3c5575be399c8c7
Alexander Gorskih <gorskihag@Alexanders-iMac.local>
2016-07-06 02:43:25 +0500

    Initialized project.

---
+++ server.js
@@ -0,0 +1,12 @@
+/* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
